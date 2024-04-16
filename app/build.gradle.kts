c5a9613d73603407ea99bb28c3d08a452dd3e8ea
yusuf kemal pınarcı <yusufkmlpinarci@gmail.com>
2024-04-16 06:56:05 +0300

    google maps işlemleri yapıldı

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin)
+
+}
+
+
+
+android {
+    namespace = "com.example.google_maps_kotlin"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.google_maps_kotlin"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+    // Maps SDK for Android KTX Library
+    implementation ("com.google.maps.android:maps-ktx:3.0.0")
+
+    // Maps SDK for Android Utility Library KTX Library
+    implementation ("com.google.maps.android:maps-utils-ktx:3.0.0")
+
+    // Lifecycle Runtime KTX Library
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+secrets {
+
+
+   
+
+    // Configure which keys should be ignored by the plugin by providing regular expressions.
+    // "sdk.dir" is ignored by default.
+    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
+}
